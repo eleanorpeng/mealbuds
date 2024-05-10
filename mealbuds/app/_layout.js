00a6d79f48a1db4730e741d4c7f915d195931e95
eleanorpeng <eleanorpeng31@gmail.com>
2024-05-09 19:32:15 -0700

    Add tab bar icons.

--- mealbuds/app/_layout.js
+++ mealbuds/app/_layout.js
@@ -1,20 +1,49 @@
-// import { Stack } from "expo-router";
-
-// export default function Layout() {
-//   return (
-//     <Stack>
-//       <Stack.Screen name="index" />
-//     </Stack>
-//   );
-// }
 import { Tabs } from "expo-router";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { Themes, Images } from "../assets/Themes";
 
 export default function Layout() {
   return (
-    <Tabs>
-      <Tabs.Screen name="index"></Tabs.Screen>
-      <Tabs.Screen name="messages"></Tabs.Screen>
-      <Tabs.Screen name="profile"></Tabs.Screen>
+    <Tabs
+      screenOptions={{
+        headerStyle: { backgroundColor: Themes.colors.grayBackground },
+        headerTitle: "MealBuds",
+        headerBackTitleStyle: {
+          fontSize: 20,
+          color: Themes.colors.orange,
+          fontFamily: "Inter-Regular",
+        },
+        headerTintColor: Themes.colors.orange,
+        tabBarActiveTintColor: Themes.colors.orange,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="home" size={size} color={color} />
+          ),
+        }}
+      ></Tabs.Screen>
+      <Tabs.Screen
+        name="messages"
+        options={{
+          tabBarLabel: "Messages",
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="chatbox-ellipses" size={size} color={color} />
+          ),
+        }}
+      ></Tabs.Screen>
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ size, color }) => (
+            <Ionicons name="person-circle" size={size} color={color} />
+          ),
+        }}
+      ></Tabs.Screen>
     </Tabs>
   );
 }
