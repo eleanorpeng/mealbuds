60128281102a1e507aa690023cf9391f914e40fd
aaron-cho <122117824+aaron-cho@users.noreply.github.com>
2024-05-11 14:56:46 -0700

    profile page

---
+++ mealbuds/app/profile/profilePage.js
@@ -0,0 +1,54 @@
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import { Themes } from "../../assets/Themes";
+import Profile from "../../components/profile";
+
+const hardcodedProfileData = {
+  name: "James Landay",
+  profilePicUrl:
+    "https://pbs.twimg.com/profile_images/1258841358220972032/MzL1iXMN_400x400.jpg",
+  major: "Computer Science",
+  year: "Postdoc",
+  interests: ["Teaching", "HCI", "design-thinking", "food", "jazz"],
+  hobbies: [
+    "Pickle ball",
+    "trying new boba spots",
+    "watching movies",
+    "sunbathing",
+  ],
+  hometown: "Lansing, Michigan",
+  diningHalls: ["Stern", "Wilbur", "Lakeside"],
+};
+
+const renderProfile = () => {
+  return (
+    <Profile
+      name={hardcodedProfileData.name}
+      profilePicUrl={hardcodedProfileData.profilePicUrl}
+      major={hardcodedProfileData.major}
+      year={hardcodedProfileData.year}
+      interests={hardcodedProfileData.interests}
+      hobbies={hardcodedProfileData.hobbies}
+      hometown={hardcodedProfileData.hometown}
+      diningHalls={hardcodedProfileData.diningHalls}
+    />
+  );
+};
+
+const ProfilePage = () => {
+  return (
+    <View style={styles.container}>
+      <Profile {...hardcodedProfileData} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    backgroundColor: Themes.colors.background,
+    gap: 20,
+  },
+});
+
+export default ProfilePage;
---
+++ mealbuds/components/profile.js
@@ -0,0 +1,333 @@
+import {
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  Pressable,
+  View,
+  Image,
+  Dimensions,
+  FlatList,
+  TouchableOpacity,
+  Modal,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { useState, useEffect } from "react";
+import { Images, Themes } from "../assets/Themes";
+
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
+const Profile = ({
+  name,
+  profilePicUrl,
+  major,
+  year,
+  interests,
+  hobbies,
+  hometown,
+  diningHalls,
+}) => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.profileContainer}>
+        <View style={styles.namePicContainer}>
+          <Text
+            style={{
+              fontSize: 24,
+              fontFamily: "Inter-Bold",
+              paddingBottom: 12,
+            }}
+          >
+            {name}
+          </Text>
+          <Image
+            style={styles.profilePic}
+            source={{
+              uri: profilePicUrl,
+            }}
+          />
+        </View>
+        <View style={styles.detailsContainer}>
+          <View style={styles.rectangle}>
+            <View style={styles.detailContainer}>
+              <View style={styles.detailHeaderContainer}>
+                <View style={styles.detailHeader}>
+                  <Text
+                    style={{
+                      fontSize: 12,
+                      fontFamily: "Inter-Bold",
+                      alignSelf: "center",
+                    }}
+                  >
+                    Major
+                  </Text>
+                </View>
+              </View>
+              <Text
+                style={{
+                  paddingTop: 8,
+                  paddingHorizontal: 8,
+                  fontSize: 12,
+                  fontFamily: "Inter",
+                  alignSelf: "center",
+                }}
+              >
+                {major}
+              </Text>
+            </View>
+
+            <View style={styles.detailContainer}>
+              <View style={styles.detailHeaderContainer}>
+                <View style={styles.detailHeader}>
+                  <Text
+                    style={{
+                      fontSize: 12,
+                      fontFamily: "Inter-Bold",
+                      alignSelf: "center",
+                    }}
+                  >
+                    Year
+                  </Text>
+                </View>
+              </View>
+              <Text
+                style={{
+                  paddingTop: 8,
+                  paddingHorizontal: 8,
+                  fontSize: 12,
+                  fontFamily: "Inter",
+                  alignSelf: "center",
+                }}
+              >
+                {year}
+              </Text>
+            </View>
+          </View>
+          <View style={styles.rectangle}>
+            <View style={styles.detailContainer}>
+              <View style={styles.detailHeaderContainer}>
+                <View style={styles.detailHeader}>
+                  <Text
+                    style={{
+                      fontSize: 12,
+                      fontFamily: "Inter-Bold",
+                      alignSelf: "center",
+                    }}
+                  >
+                    Interests
+                  </Text>
+                </View>
+              </View>
+              <Text
+                style={{
+                  paddingTop: 8,
+                  paddingHorizontal: 8,
+                  fontSize: 12,
+                  fontFamily: "Inter",
+                  alignSelf: "center",
+                }}
+              >
+                {interests.join(", ")}
+              </Text>
+            </View>
+
+            <View style={styles.detailContainer}>
+              <View style={styles.detailHeaderContainer}>
+                <View style={styles.detailHeader}>
+                  <Text
+                    style={{
+                      fontSize: 12,
+                      fontFamily: "Inter-Bold",
+                      alignSelf: "center",
+                    }}
+                  >
+                    Hobbies
+                  </Text>
+                </View>
+              </View>
+              <Text
+                style={{
+                  paddingTop: 8,
+                  paddingHorizontal: 8,
+                  fontSize: 12,
+                  fontFamily: "Inter",
+                  alignSelf: "center",
+                }}
+              >
+                {hobbies.join(", ")}
+              </Text>
+            </View>
+          </View>
+          <View style={styles.rectangle}>
+            <View style={styles.detailContainer}>
+              <View style={styles.detailHeaderContainer}>
+                <View style={styles.detailHeader}>
+                  <Text
+                    style={{
+                      fontSize: 12,
+                      fontFamily: "Inter-Bold",
+                      alignSelf: "center",
+                    }}
+                  >
+                    Hometown
+                  </Text>
+                </View>
+              </View>
+              <Text
+                style={{
+                  paddingTop: 8,
+                  paddingHorizontal: 8,
+                  fontSize: 12,
+                  fontFamily: "Inter",
+                  alignSelf: "center",
+                }}
+              >
+                {hometown}
+              </Text>
+            </View>
+
+            <View style={styles.detailContainer}>
+              <View style={styles.detailHeaderContainer}>
+                <View style={styles.detailHeader}>
+                  <Text
+                    style={{
+                      fontSize: 12,
+                      fontFamily: "Inter-Bold",
+                      alignSelf: "center",
+                    }}
+                  >
+                    Preferred Dining Halls
+                  </Text>
+                </View>
+              </View>
+              <Text
+                style={{
+                  paddingTop: 8,
+                  paddingHorizontal: 8,
+                  fontSize: 12,
+                  fontFamily: "Inter",
+                  alignSelf: "center",
+                }}
+              >
+                {diningHalls.join(", ")}
+              </Text>
+            </View>
+          </View>
+        </View>
+      </View>
+      <View style={styles.editButtonsContainer}>
+        <TouchableOpacity style={styles.editButton}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontFamily: "Inter",
+              alignSelf: "center",
+            }}
+          >
+            Edit Profile
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.editButton}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontFamily: "Inter",
+              alignSelf: "center",
+            }}
+          >
+            Edit Availability
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+    backgroundColor: Themes.colors.background,
+    alignItems: "center",
+    paddingTop: 24,
+  },
+  profileContainer: {
+    width: windowWidth * 0.88,
+    height: windowHeight * 0.6,
+    borderRadius: "50%",
+    alignContent: "center",
+    flexDirection: "column",
+    // borderColor: "red",
+    // borderWidth: 1,
+    padding: 16,
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOffset: { width: 3, height: 3 },
+    shadowOpacity: 0.05,
+    shadowRadius: 2,
+  },
+  namePicContainer: {
+    alignItems: "center",
+    // borderColor: "red",
+    // borderWidth: 1,
+  },
+  profilePic: {
+    width: windowWidth * 0.35,
+    height: windowWidth * 0.35,
+    borderRadius: (windowWidth * 0.35) / 2,
+  },
+  detailsContainer: {
+    flex: 1,
+    paddingTop: 8,
+    // borderColor: "red",
+    // borderWidth: 1,
+  },
+  rectangle: {
+    flex: 1,
+    flexDirection: "row",
+    marginVertical: 4,
+    // borderColor: "red",
+    // borderWidth: 1,
+  },
+  detailContainer: {
+    flex: 1,
+    // borderColor: "green",
+    // borderWidth: 1,
+  },
+  detailHeaderContainer: {
+    height: "40%",
+    paddingTop: 8,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    // borderColor: "blue",
+    // borderWidth: 1,
+  },
+  detailHeader: {
+    backgroundColor: Themes.colors.backgroundOrange,
+    borderRadius: "50%",
+    flex: 1,
+    paddingHorizontal: 12,
+    justifyContent: "center",
+  },
+  editButtonsContainer: {
+    flex: 1,
+    flexDirection: "row",
+    width: windowWidth,
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    // borderColor: "blue",
+    // borderWidth: 1,
+  },
+  editButton: {
+    height: "50%",
+    width: "40%",
+    backgroundColor: Themes.colors.backgroundOrange,
+    borderRadius: "50%",
+    paddingHorizontal: 12,
+    justifyContent: "center",
+  },
+});
+
+export default Profile;
